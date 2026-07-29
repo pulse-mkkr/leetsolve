228991076455f9e084f6e012c10a6d45101e8bc3
Manish <manishkkr071083@gmail.com>
2026-07-29 22:51:33 +0530

    Create 704.cpp

---
+++ cppcourse/704.cpp
@@ -0,0 +1,15 @@
+//Binary Search
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int lo=0,hi=n-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<target)lo=mid+1;
+            else hi=mid-1;
+        }
+        return -1;
+    }
+};
