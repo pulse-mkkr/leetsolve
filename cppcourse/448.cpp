f01b1154525022fb1bd2d711a5539dff8acc9c44
Manish <manishkkr071083@gmail.com>
2026-08-12 12:28:40 +0530

    Create 448.cpp

---
+++ cppcourse/448.cpp
@@ -0,0 +1,17 @@
+//448. Find All Numbers Disappeared in an Array
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for(int i=0;i<n;){
+            if(nums[i]!=nums[nums[i]-1]) swap(nums[i],nums[nums[i]-1]);
+            else i++;
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1)ans.push_back(i+1);
+        }
+        return ans;
+    }
+};
