09bca72373ba648121a59657e9d4036487b0b295
Manish <manishkkr071083@gmail.com>
2026-08-30 23:27:04 +0530

    Create 507.cpp

---
+++ cppcourse2/sievemath/507.cpp
@@ -0,0 +1,17 @@
+//507. Perfect Number
+
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num==1)return false;
+        int sum=1;
+        for(int i=2;i<=sqrt(num);i++){
+            if(num%i==0){
+                sum+=i;
+                sum+=(num/i);
+            }
+            if(sum>num)return false;
+        }
+        return (sum==num);
+    }
+};
