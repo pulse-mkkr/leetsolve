648154468ed1d08b850874fdc781655d584c3682
Manish <manishkkr071083@gmail.com>
2026-09-18 22:01:05 +0530

    Create 141.cpp

---
+++ cppcourse2/linkedlist/141.cpp
@@ -0,0 +1,15 @@
+//141. Linked List Cycle
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast!=NULL&&fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast)return true;
+        }
+        return false;
+    }
+};
