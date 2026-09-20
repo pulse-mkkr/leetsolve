870049b91b3e998c354f9fea27f7ea14dcba1a82
Manish <manishkkr071083@gmail.com>
2026-09-20 22:43:49 +0530

    Create w520p1.cpp

---
+++ Contests/w520p1.cpp
@@ -0,0 +1,15 @@
+//4056. Number of Intersecting Interval Pairs I
+
+class Solution {
+public:
+    int countIntersectingIntervals(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(max(intervals[i][0],intervals[j][0])<=min(intervals[i][1],intervals[j][1]))ans++;
+            }
+        }
+        return ans;
+    }
+};
