0da890d501bff05947007be36c49c13bf19e0071
Manish <manishkkr071083@gmail.com>
2026-09-21 22:30:25 +0530

    Create 83.cpp

---
+++ cppcourse2/linkedlist/83.cpp
@@ -0,0 +1,13 @@
+//83. Remove Duplicates from Sorted List
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp=head;
+        while(temp!=NULL&&temp->next!=NULL){
+            if(temp->val==temp->next->val)temp->next=temp->next->next;
+            else temp=temp->next;
+        }
+        return head;
+    }
+};
